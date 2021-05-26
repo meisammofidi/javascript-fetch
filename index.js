9d71f249491b1a47e5c9dbef87f6c21d2e8e01b1
meisammofidi <meisam.mofidi@yahoo.com>
2021-05-25 23:32:51 -0400

    Added new implementation by Async/Await

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 //By default fetch using GET method
-function getAll() {
+async function getAll() {
+  // solution 1 - Using promise with then
   fetch('https://jsonplaceholder.typicode.com/todos')
     .then((res) => res.json())
     .then((data) => console.log(data));
+
+  // solution 2 - Using Async/await
+  try {
+    const result = await fetch('https://jsonplaceholder.typicode.com/todos');
+    const jesult = await result.json();
+    console.log(jesult);
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
-function getUserById(Id) {
+async function getUserById(Id) {
   fetch(`https://jsonplaceholder.typicode.com/todos/${Id}`)
     // .then((res) => {
     //   if (res.ok) console.log('SUCCESS');
     //   else console.log(`FAILED. There is no item with ${Id}`);
     // })
-    .then(res => res.json())
+    .then((res) => res.json())
     .then((data) => console.log(data));
+
+  // solution 2 - Using Async/await
+  try {
+    const result = await fetch(
+      `https://jsonplaceholder.typicode.com/todos/${Id}`
+    );
+    if (!result.ok)
+      throw new Error(`Request failed with status (${result.status})`);
+    const jesult = await result.json();
+    console.log(jesult);
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
-function insertItem() {
+async function insertItem() {
   fetch('https://jsonplaceholder.typicode.com/todos', {
     method: 'POST',
     headers: {
@@ -29,11 +52,31 @@ function insertItem() {
     }),
   })
     .then((res) => res.json())
-    .then((data) =>
-      console.log(`insert done successfully, ${data}`)
+    .then((data) => console.log(`insert done successfully, ${JSON.stringify(data)}`));
+
+  try {
+    const insertedItem = await fetch(
+      'https://jsonplaceholder.typicode.com/todos',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          userId: 1,
+          id: 10,
+          title: 'A chorse at home',
+          completed: true,
+        }),
+      }
     );
+
+    console.log(insertedItem);
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
-getAll();
-getUserById(8);
+// getAll();
+// getUserById(8);
 insertItem();
